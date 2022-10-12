2c5ae6abc981c888f8c77a38253d8bef60fa6963
Mitrasish Mukherjee <mmitrasish97@gmail.com>
2022-10-12 14:05:48 +0530

    Update grant factory vesting contract

--- src/config/addresses.ts
+++ src/config/addresses.ts
@@ -1,6 +1,6 @@
 const addresses = {
-    argoERC20: "0x28cca76f6e8ec81e4550ecd761f899110b060e97",
-    argoVestingFactory: "0x2C11c8A708caea4512A3aD365800F20A0952dc0f"
+  argoERC20: "0x28cca76f6e8ec81e4550ecd761f899110b060e97",
+  argoVestingFactory: "0x1078102BE89b339E316130841F61034746Bc95Bc",
 };
 
 export default addresses;
